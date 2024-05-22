e8e7dfefc507b71bf13885a9a80a6b55fcbbd70b
Fiqri317 <139039571+Fiqri317@users.noreply.github.com>
2024-05-22 19:16:39 +0700

    Update BM.cs

--- Tubes3_X/Tubes3_X/Helpers/BM.cs
+++ Tubes3_X/Tubes3_X/Helpers/BM.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace Tubes3_X
 {
     class BM
@@ -5,39 +7,38 @@ namespace Tubes3_X
         public static int Handler(string pattern, string text)
         {
             int m = pattern.Length;
-    int n = text.Length;
+            int n = text.Length;
 
-    int[] badChar = new int[256];
-    BadCharHeuristic(pattern, m, ref badChar);
+            int[] badChar = new int[256];
+            Handler(pattern, m, ref badChar);
 
-    int s = 0;
-    while (s <= (n - m))
-    {
-        int j = m - 1;
-
-        while (j >= 0 && pattern[j] == text[s + j])
-            j--;
-
-        if (j < 0)
-        {
-            return s; // Match found at index s
+            int s = 0;
+            while (s <= (n - m))
+            {
+                int j = m - 1;
+                
+                while (j >= 0 && pattern[j] == text[s + j])
+                j--;
+                
+                if (j < 0)
+                {
+                    return s; 
+                }
+                else
+                {
+                    s += Math.Max(1, j - badChar[(int)text[s + j]]);
+                }
+            }
+            return -1; 
         }
-        else
+
+        private static void Handler(string str, int size, ref int[] badChar)
         {
-            s += Math.Max(1, j - badChar[(int)text[s + j]]);
-        }
-    }
-    return -1; // No match found
-}
+            for (int i = 0; i < 256; i++)
+            badChar[i] = -1;
 
-private static void BadCharHeuristic(string str, int size, ref int[] badChar)
-{
-    for (int i = 0; i < 256; i++)
-        badChar[i] = -1;
-
-    for (int i = 0; i < size; i++)
-        badChar[(int)str[i]] = i;
-}
+            for (int i = 0; i < size; i++)
+            badChar[(int)str[i]] = i;
         }
     }
 }
