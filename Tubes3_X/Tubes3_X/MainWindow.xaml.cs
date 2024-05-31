4772b7ae16a7498fcf05e2bc56fc18af4a71aa7a
WazeAzure <bryan.rich0604@gmail.com>
2024-05-31 13:16:30 +0700

    fix: relative path

--- Tubes3_X/Tubes3_X/MainWindow.xaml.cs
+++ Tubes3_X/Tubes3_X/MainWindow.xaml.cs
@@ -19,7 +19,11 @@ namespace Tubes3_X
     {
         public MainWindow()
         {
-            InitializeComponent();
+            try {
+                InitializeComponent();
+            } catch (Exception e) {
+                Console.WriteLine(e);
+            }
             Trace.WriteLine("text");
             string folder = "../../../Test/SOCOFing/SOCOFing/Real/";
             string fileSrc = folder + "1__M_Left_index_finger.bmp";
