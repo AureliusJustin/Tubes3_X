85e6f524f0eb627ce9ee3374cbdae72de27a9010
dhiabziz <dhiditabdi@gmail.com>
2024-06-08 13:41:59 +0700

    feat: show biodata

--- Tubes3_X/Tubes3_X/MainWindow.xaml.cs
+++ Tubes3_X/Tubes3_X/MainWindow.xaml.cs
@@ -12,7 +12,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Text.RegularExpressions;
+using System.Drawing;
 using System.IO;
+using System.Windows.Forms;
+
 
 namespace Tubes3_X
 {
@@ -25,11 +28,15 @@ namespace Tubes3_X
         private string inputFilePath = "";
         private string inputAlgo = "";
         private Algorithm algo;
+
+
         public MainWindow()
         {
             try {
                 // initialize UI
                 InitializeComponent();
+
+                
 
                 // initialize Database
                 this.db = new Database();
@@ -38,10 +45,15 @@ namespace Tubes3_X
                 // initialize algorithm
                 this.algo = new Algorithm(this.db);
 
+                
+
             } catch (Exception e) {
                 Console.WriteLine(e);
             }
+            
         }
+
+        
 
         public void WindowExit(object sender, System.ComponentModel.CancelEventArgs e)
         {
@@ -54,6 +66,28 @@ namespace Tubes3_X
             // search button clicked
             // this.inputFilePath = folder + "250__F_Right_ring_finger.BMP";
             // this.inputFilePath = "../../../Test/SOCOFing/Altered/Altered-Medium/" + "2__F_Left_index_finger_Obl.BMP";
+            Label lnama = (Label)Nama;
+            Label ltempatlahir = (Label)TempatLahir;
+            Label ltanggallahir = (Label)TanggalLahir;
+            Label lkelamin = (Label)Kelamin;
+            Label lgoldar = (Label)Goldar;
+            Label lalamat = (Label)Alamat;
+            Label lagama = (Label)Agama;
+            Label lstatus = (Label)Status;
+            Label lpekerjaan = (Label)Pekerjaan;
+            Label lkewarganegaraan = (Label)Kewarganegaraan;
+
+            lnama.Content = "";
+            ltempatlahir.Content = "";
+            ltanggallahir.Content = "";
+            lkelamin.Content = "";
+            lgoldar.Content = "";
+            lalamat.Content = "";
+            lagama.Content = "";
+            lstatus.Content = "";
+            lpekerjaan.Content = "";
+            lkewarganegaraan.Content = "";
+
             string result = this.algo.AlgoMain(this.inputFilePath, "BM");
 
             if(result != ""){
@@ -71,6 +105,30 @@ namespace Tubes3_X
                 }
 
                 Biodata baru = db.getBiodataFromName(namaAlay);
+                
+
+                lnama.Content = baru.nama;
+                ltempatlahir.Content = baru.tempatLahir;
+                ltanggallahir.Content = baru.tanggalLahir;
+                lkelamin.Content = baru.kelamin;
+                lgoldar.Content = baru.goldar;
+                lalamat.Content = baru.alamat;
+                lagama.Content = baru.agama;
+                lstatus.Content = baru.statusPerkawinan;
+                lpekerjaan.Content = baru.pekerjaan;
+                lkewarganegaraan.Content = baru.kewarganegaraan;
+
+
+
+
+
+
+
+
+                /*      Label Kw = new Label();
+                      Kw = (Label)this.FindControl("Kewarganegaraan" + i.ToString()) as Label;
+                      Kw.Text = "wkwk";*/
+
                 Console.WriteLine(baru.nama);
                 Console.WriteLine(baru.nik);
                 
@@ -85,6 +143,7 @@ namespace Tubes3_X
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             ComboBoxItem selectedItem = (ComboBoxItem) ComboBox1.SelectedItem;
+            
 
             if (selectedItem != null)
             {
